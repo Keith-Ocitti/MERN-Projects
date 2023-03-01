ec601ff56f1fe692de24a71cc58dcccb1916af54
Keith-Ocitti <keithdimaria@gmail.com>
2023-03-01 14:33:45 +0300

    Update products.js

--- e-commerce-api/controllers/products.js
+++ e-commerce-api/controllers/products.js
@@ -2,11 +2,13 @@ const { BadRequestError } = require("../errors/index");
 const Product = require("../models/product");
 const { StatusCodes } = require("http-status-codes");
 
+// function to get all products
 const getAllProducts = async (req, res) => {
   const product = await Product.find({});
   res.status(StatusCodes.OK).json({ product });
 };
 
+// function to get a single product
 const getSpecificProduct = async (req, res) => {
   const { id: productId } = req.params;
   const product = await Product.findById(productId);
